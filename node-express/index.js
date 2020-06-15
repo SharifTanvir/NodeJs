e564cfefd20aa496efc78ae1f7b5ff96ea36c33e
SharifTanvir <mislam151243@bscse.uiu.ac.bd>
2020-06-15 11:56:03 +0600

    assignment-1

--- node-express/index.js
+++ node-express/index.js
@@ -1,72 +1,53 @@
-const express = require('express'),
-    http = require('http');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-const morgan = require('morgan');
+var hostname = 'localhost';
+var port = 3000;
 
-const hostname = 'localhost';
-const port = 3000;
-const dishRouter = require('./routes/dishRouter');
-const app = express();
-const bodyParser = require('body-parser');
+var dishRouter = require('./route/dishRouter');
+var leaderRouter = require('./route/leaderRouter');
+var promoRouter = require('./route/promoRouter');
 
-app.use(morgan('dev'));
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
+app.use('/leadership', leaderRouter);
+app.use('/promotions', promoRouter);
 
-app.all('/dishes', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname, function(){
+    console.log('Server running at http://'+hostname+':'+port);
+})
